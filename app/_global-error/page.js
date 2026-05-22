31e48fdb43a4f28ff88443f6a60b89cf93cc1b09
mikhunasaas <mikhunasaas@gmail.com>
2026-05-22 00:37:52 -0400

    update

--- app/_global-error/page.js
+++
@@ -1,9 +0,0 @@
-var R=require("../../chunks/ssr/[turbopack]_runtime.js")("server/app/_global-error/page.js")
-R.c("server/chunks/ssr/[root-of-the-server]__0n-4jx1._.js")
-R.c("server/chunks/ssr/0prs_next_dist_esm_build_templates_app-page_0v~n4v7.js")
-R.c("server/chunks/ssr/[root-of-the-server]__0bp2mww._.js")
-R.c("server/chunks/ssr/[root-of-the-server]__0i1yhln._.js")
-R.c("server/chunks/ssr/0prs_next_dist_client_components_builtin_global-error_08dhpo7.js")
-R.c("server/chunks/ssr/_next-internal_server_app__global-error_page_actions_0k77kol.js")
-R.m(3856)
-module.exports=R.m(3856).exports
