92a8857a7f2de110df8d2f8ac6346407547ee5b3
mikhunasaas <mikhunasaas@gmail.com>
2026-05-22 00:47:50 -0400

    update

--- index.segments/_tree.segment.rsc
+++
@@ -1,3 +0,0 @@
-:HL["/_next/static/chunks/0hlx040j9mmc6.css","style"]
-:HL["https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.14.0/css/all.min.css","style",{"crossOrigin":"anonymous","integrity":"sha512-1PKOgIY59xJ8Co8+NE6FZ+LOAZKjy+KY8iq0G4B3CyeY6wYHN3yt9PW0XpSriVlkMXe40PTKnXrLnZ9+fkDaog=="}]
-0:{"tree":{"name":"","param":null,"prefetchHints":16,"slots":{"children":{"name":"__PAGE__","param":null,"prefetchHints":0,"slots":null}}},"staleTime":300,"buildId":"0H2-I2AqLxoihKzNXL5Zb"}
---
+++ middleware-build-manifest.js
@@ -0,0 +1,20 @@
+globalThis.__BUILD_MANIFEST = {
+  "pages": {
+    "/_app": []
+  },
+  "devFiles": [],
+  "polyfillFiles": [
+    "static/chunks/03~yq9q893hmn.js"
+  ],
+  "lowPriorityFiles": [
+    "static/GSOiTfYnxNEXvH_FccN_1/_buildManifest.js",
+    "static/GSOiTfYnxNEXvH_FccN_1/_ssgManifest.js",
+    "static/GSOiTfYnxNEXvH_FccN_1/_clientMiddlewareManifest.js"
+  ],
+  "rootMainFiles": [
+    "static/chunks/0q-p3p4489r1w.js",
+    "static/chunks/0kk1-zsz0vvoe.js",
+    "static/chunks/0n_bo8lpxfr8q.js",
+    "static/chunks/turbopack-0i.k8gxf13cag.js"
+  ]
+};
